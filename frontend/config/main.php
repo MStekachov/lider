b176c6f7c9b8807ee82b0278884074cfe7abd1db
mstekachov <mstekachov@gmail.com>
2022-10-14 14:35:32 +0300

    synchronized breches

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -50,7 +50,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                '' => 'site/index',                                
+                '' => '',                                
                 '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
